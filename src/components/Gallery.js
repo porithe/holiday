00b67b8b9946be1c3a6e9fffafa7f28b8fd02d5c
porithe <malkowski.buisness@gmail.com>
2019-04-18 16:02:18 +0200

    photos on gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -30,15 +30,15 @@ class Gallery extends React.Component {
                     <h1>Galeria</h1>
                 </div>
                 <div className="gallery-images">
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
-                    <img onClick={ () => this.zoomImage('/static/media/room.bc174531.jpg')} src={require('../images/room.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture1.bc174531.jpg')} src={require('../images/picture1.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture2.3a5c2279.jpg')} src={require('../images/picture2.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture3.fed55d5a.jpg')} src={require('../images/picture3.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture4.ef038863.jpg')} src={require('../images/picture4.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture5.0fa51d8c.jpg')} src={require('../images/picture5.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture6.95d05bc7.jpg')} src={require('../images/picture6.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture7.16988a04.jpg')} src={require('../images/picture7.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture8.04778605.jpg')} src={require('../images/picture8.jpg')} alt={"zdjecie"} />
+                    <img onClick={ () => this.zoomImage('/static/media/picture9.e200b07c.jpg')} src={require('../images/picture9.jpg')} alt={"zdjecie"} />
                 </div>
             </div>
         )
